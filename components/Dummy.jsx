3f7d0d288d9d3fac64df374676ff33f5c12d8909
Akhil-Polisetty <polisettiakhi72073@gmail.com>
2024-02-29 20:56:17 +0530

    this is my first deployement

---
+++ components/Dummy.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './dummy.css'
+const Dummy = () => {
+  return (
+    <div className='dummy-cont'>
+        <div className='cont'>
+            Hello Guys welcome to my first deployed project
+        </div>
+    </div>
+  )
+}
+
+export default Dummy
